db5c873fdc98e9913925d5de281b469d1c8c7430
jennyferparmar16-yel <jenniparmar7@gmail.com>
2026-02-15 23:20:42 -0500

    Profile.kt
    Defines a data class used to store profile information, including a user's name and age, which is used throughout the application.

---
+++ Profile.kt
@@ -0,0 +1,21 @@
+/*
+Course: MAD 302
+Lab: Lab 1
+Name: Jennyfer Parmar
+Student ID: A00201240
+Date: 13 February 2026
+Description:
+Data class representing a profile containing name and age.
+*/
+
+package com.example.lab1_mad301_jennyfer
+
+/*
+Profile stores user information used in the app.
+name: user's name
+age: user's age
+*/
+data class Profile(
+    val name: String,
+    val age: Int
+)
